a3339d2c0d3dbbb83e79154bd7e5efe0120d28be
Mario Blas <marioblasgc@gmail.com>
2014-11-04 14:11:20 +0000

    Syntax highlighting

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -1,3 +1,10 @@
 Meteor.startup(function() {
+	// Moment.js
 	moment.locale('es');
+
+	// Highlight.js
+	hljs.configure({
+		tabReplace: '    ', // 4 spaces
+		useBR: true
+	});
 });
--- client/views/blog/article.js
+++ client/views/blog/article.js
@@ -1,3 +1,10 @@
+Template.article.rendered = function() {
+	// Highlight.js
+	$('pre code').each(function(i, block) {
+		hljs.highlightBlock(block);
+	});
+};
+
 Template.article.helpers({
 	articles: function() {
 		return Articles.find();
